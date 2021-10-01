d50c19e27209a72e9379a553bcb2b5fcb67f3c99
Prakash Goud <prakash.goud@morningstar.com>
2021-10-02 00:04:10 +0530

    [Prakash] - Provided user friendly error response

--- src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionHandler.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.addressbookapp.exceptions;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -6,19 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.bridgelabz.addressbookapp.model.Response;
 
-@ControllerAdvice
-public class AddressBookException {
+public class AddressBookExceptionHandler {
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Response> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+	public ResponseEntity<Response> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
 		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-		List<String> errMsg = errorList.stream().map(objErr->objErr.getDefaultMessage()).collect(Collectors.toList());
-		Response resDTO = new Response("Exception while processing rest request",errMsg);
-		return new ResponseEntity<Response>(resDTO,HttpStatus.BAD_REQUEST);
+		List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+		Response resDTO = new Response("Exception while processing rest request", errMsg);
+		return new ResponseEntity<Response>(resDTO, HttpStatus.BAD_REQUEST);
 	}
+
 }
-	
